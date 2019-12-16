552de673cebc28fd1c218b68332646f00bc047b4
Saulo Maia <saulommaia@gmail.com>
2019-12-16 11:40:38 -0300

    Update main.js

--- src/jupyter_contrib_nbextensions/nbextensions/setup/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/setup/main.js
@@ -2,7 +2,7 @@ define(['base/js/namespace', 'base/js/events'], function (Jupyter, events) {
   // Template cells including markdown and imports
   var setUp = function () {
     Jupyter.notebook.insert_cell_at_index('markdown', 0)
-      .set_text(`# Informações Gerais')
+      .set_text(`# Informações Gerais`)
     Jupyter.notebook.insert_cell_at_index('markdown', 1)
       .set_text(`
 - Nome do desenvolvedor: 
@@ -18,7 +18,7 @@ define(['base/js/namespace', 'base/js/events'], function (Jupyter, events) {
 - Resumo da aplicação: 
     - -
 - Outras observações: 
-    - -')
+    - -`)
 
     Jupyter.notebook.insert_cell_at_index('markdown', 1).set_text(`### Imports
 Import libraries and write settings here.`)
